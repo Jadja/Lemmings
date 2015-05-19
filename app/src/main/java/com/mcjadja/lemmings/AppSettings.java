a88584a7cd01e5272e10df43d250c640b178463e
Jadja <jadjadebeste@gmail.com>
2015-05-19 10:11:54 +0200

    Getting started

---
+++ app/src/main/java/com/mcjadja/lemmings/AppSettings.java
@@ -0,0 +1,15 @@
+package com.mcjadja.lemmings;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jadja on 5/19/2015.
+ */
+public class AppSettings extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_settings);
+    }
+}
--- app/src/main/java/com/mcjadja/lemmings/MainActivity.java
+++ app/src/main/java/com/mcjadja/lemmings/MainActivity.java
@@ -1,17 +1,33 @@
 package com.mcjadja.lemmings;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doStart(View v) {
+        //Starting a new Intent
+        Intent nextScreen = new Intent(getApplicationContext(), LevelSelect.class);
+
+        startActivity(nextScreen);
+    }
+    public void doSettings(View v) {
+        //Starting a new Intent
+        Intent nextScreen = new Intent(getApplicationContext(), AppSettings.class);
+
+        startActivity(nextScreen);
     }
 
 }
