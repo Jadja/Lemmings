06e01b5663e0538a347421d999e2d4f79841eadc
Jadja <jadjadebeste@gmail.com>
2015-05-31 15:08:05 +0200

    Menu update
    Great changes to the menu and the settings screen

--- app/src/main/java/com/mcjadja/lemmings/AppSettings.java
+++ app/src/main/java/com/mcjadja/lemmings/AppSettings.java
@@ -1,15 +1,61 @@
 package com.mcjadja.lemmings;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Jadja on 5/19/2015.
  */
 public class AppSettings extends Activity {
+
+    boolean music = true;
+    boolean gamesounds = true;
+    boolean vibration = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.app_settings);
     }
+
+    public void doMenu(View v) {
+        //Starting a new Intent
+        Intent nextScreen = new Intent(getApplicationContext(), MainActivity.class);
+
+        startActivity(nextScreen);
+
+    }
+    public void toggleMusic(View v) {
+        //Starting a new Intent
+        if (music)    {
+            music = false;
+        }
+        else    {
+            music = true;
+        }
+    }
+
+    public void toggleGamesounds(View v) {
+        //Starting a new Intent
+        if (gamesounds)    {
+            gamesounds = false;
+        }
+        else    {
+            gamesounds = true;
+        }
+    }
+
+    public void toggleVibrate(View v) {
+        //Starting a new Intent
+        if (vibration)    {
+            vibration = false;
+        }
+        else    {
+            vibration = true;
+        }
+    }
+
 }
--- app/src/main/java/com/mcjadja/lemmings/LevelSelect.java
+++ app/src/main/java/com/mcjadja/lemmings/LevelSelect.java
@@ -1,15 +1,21 @@
 package com.mcjadja.lemmings;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Jadja on 5/19/2015.
  */
 public class LevelSelect extends Activity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.level_select);
     }
+
+
 }
