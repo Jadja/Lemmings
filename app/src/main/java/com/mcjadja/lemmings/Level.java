84a629a884b8fa7515381d6c3120351071e51fc0
Jadja <jadjadebeste@gmail.com>
2015-05-31 15:56:34 +0200

    Level select menu
    That totally does not work but aesthetics man

---
+++ app/src/main/java/com/mcjadja/lemmings/Level.java
@@ -0,0 +1,15 @@
+package com.mcjadja.lemmings;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jadja on 5/31/2015.
+ */
+public class Level extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.level);
+    }
+}
--- app/src/main/java/com/mcjadja/lemmings/LevelSelect.java
+++ app/src/main/java/com/mcjadja/lemmings/LevelSelect.java
@@ -9,12 +9,16 @@ import android.view.View;
  * Created by Jadja on 5/19/2015.
  */
 public class LevelSelect extends Activity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.level_select);
+    }
+    public void doLevel(View v) {
+        //Starting a new Intent
+        Intent nextScreen = new Intent(getApplicationContext(), Level.class);
+
+        startActivity(nextScreen);
     }
 
 
