c39916678c5e1326ade906c147dba597673f07b2
Jadja Desktop <jadjadebeste@gmail.com>
2015-06-17 20:35:42 +0200

    AndEngine works
    Now show's a black screen. Now to implement the game

--- app/src/main/java/com/mcjadja/lemmings/Level.java
+++ app/src/main/java/com/mcjadja/lemmings/Level.java
@@ -1,8 +1,13 @@
 package com.mcjadja.lemmings;
 
+import org.andengine.engine.FixedStepEngine;
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.util.FPSLogger;
 import org.andengine.ui.IGameInterface;
 import org.andengine.ui.activity.BaseGameActivity;
 
@@ -23,7 +28,8 @@ public class Level extends BaseGameActivity {
     @Override
     public EngineOptions onCreateEngineOptions() {
         this.myCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-        return null;
+
+        return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.myCamera);
     }
 
     @Override
@@ -33,7 +39,10 @@ public class Level extends BaseGameActivity {
 
     @Override
     public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws Exception {
+        this.mEngine.registerUpdateHandler(new FPSLogger());
 
+        final Scene scene = new Scene();
+        scene.setBackground(new Background(0.09804f, 0.62074f, 0.8784f));
     }
 
     @Override
