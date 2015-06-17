93acbcc90d067e01c4021d54ce372ec57715705d
Jadja Desktop <jadjadebeste@gmail.com>
2015-06-17 19:53:22 +0200

    Start game development

--- app/src/main/java/com/mcjadja/lemmings/Level.java
+++ app/src/main/java/com/mcjadja/lemmings/Level.java
@@ -1,15 +1,43 @@
 package com.mcjadja.lemmings;
 
-import android.app.Activity;
-import android.os.Bundle;
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.IGameInterface;
+import org.andengine.ui.activity.BaseGameActivity;
 
 /**
  * Created by Jadja on 5/31/2015.
  */
-public class Level extends Activity {
+public class Level extends BaseGameActivity {
+
+    //Constants:
+
+    private static final int CAMERA_WIDTH = 800;
+    private static final int CAMERA_HEIGHT = 480;
+
+    //Fields:
+
+    private Camera myCamera;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.level);
+    public EngineOptions onCreateEngineOptions() {
+        this.myCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        return null;
+    }
+
+    @Override
+    public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws Exception {
+
+    }
+
+    @Override
+    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws Exception {
+
+    }
+
+    @Override
+    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+
     }
 }
