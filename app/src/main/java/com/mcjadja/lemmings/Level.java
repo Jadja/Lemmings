5459e1ecae2f557fbef15f89625a5d53d22291f5
Jadja Laptop <jadjadebeste@gmail.com>
2015-06-22 20:12:16 +0200

    Changes

--- app/src/main/java/com/mcjadja/lemmings/Level.java
+++ app/src/main/java/com/mcjadja/lemmings/Level.java
@@ -82,6 +82,7 @@ public class Level extends SimpleBaseGameActivity implements ButtonSprite.OnClic
     protected Scene onCreateScene() {
         final Scene scene = new Scene();
         scene.setBackground(new Background(1.000f, 0.000f, 0.000f));
+        scene.setTouchAreaBindingOnActionDownEnabled(true);
         return scene;
     }
 
